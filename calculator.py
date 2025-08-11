43487627007ab46007e1098dec94b69d96fe3d57
FuryLusifer <piyushkayastha2@gmail.com>
2025-08-11 16:55:56 +0545

    calculator skeleton

---
+++ calculator.py
@@ -0,0 +1,29 @@
+class Calculator:
+
+    def add(self):
+        pass
+
+    def subtract(self):
+        pass
+
+    def multiply(self):
+        pass
+
+    def division(self):
+        pass
+
+    def modulo(self):
+        pass
+
+    def power(self):
+        pass
+
+    def root(self):
+        pass
+
+    def run(self):
+        print("Calculator Launched!")
+
+if __name__ == "__main__":
+    calc = Calculator()
+    calc.run()
